a3de3becdbee76d355f3ba458b96d72c328685eb
luishernandez0105 <luisfelipegilhernandez@gmail.com>
2023-06-29 19:50:07 -0500

    API - Ajustes varios

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+const api = require('./api');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("El servidor esta escuchando en el puerto: " + port);
@@ -9,3 +12,5 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("El famoso hola mundo");
 });
+
+app.use('/api', api);
